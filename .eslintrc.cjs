07b6b0e5734a981c3348965aa9ee38eb3de5afed
Nobody-123 <dikhyantkrishnadalai@gmail.com>
2024-03-17 19:15:20 +0530

    Turned off all eslint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -12,32 +12,33 @@ const config = {
     "plugin:@typescript-eslint/recommended-type-checked",
     "plugin:@typescript-eslint/stylistic-type-checked"
   ],
-  "rules": {
+  /* "rules": {
     "@typescript-eslint/array-type": "off",
     "@typescript-eslint/consistent-type-definitions": "off",
-    "@typescript-eslint/consistent-type-imports": "off" /* [
+    "@typescript-eslint/consistent-type-imports": [
       "warn",
       {
         "prefer": "type-imports",
         "fixStyle": "inline-type-imports"
       }
-    ] */,
-    "@typescript-eslint/no-unused-vars": "off" /* [
+    ],
+    "@typescript-eslint/no-unused-vars": [
       "warn",
       {
         "argsIgnorePattern": "^_"
       }
-    ] */,
+    ],
     "@typescript-eslint/require-await": "off",
-    "@typescript-eslint/no-misused-promises": "off" /* [
+    "@typescript-eslint/no-misused-promises": [
       "error",
       {
         "checksVoidReturn": {
           "attributes": false
         }
       }
-    ] */
-  },
+    ]
+  }, */
+  "rules": {}
 
 }
 module.exports = config;
